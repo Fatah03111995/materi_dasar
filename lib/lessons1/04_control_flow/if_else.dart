693e65150bba43075ff8369f0fa34c027b445aa0
Abdul Fatah <fatah03111995@gmail.com>
2024-07-26 10:52:27 +0700

    feat : if else

--- lib/lessons1/04_control_flow/if_else.dart
+++ lib/lessons1/04_control_flow/if_else.dart
@@ -2,3 +2,23 @@
 // MISAL. JIKA ADA PEREMPATAN JALAN, CONTROL FLOW AKAN MEMBERIKAN LOGIKA
 // SEHINGGA PROGRAM TAU HARUS MELANJUTKAN KE KANAN, KE KIRI, ATAU LURUS
 
+// ignore_for_file: avoid_print
+
+import 'dart:math';
+
+void main(List<String> args) {
+  final random = Random();
+  int angkaRandom = random.nextInt(5); //5 adalah nilai maksimal
+
+  if (angkaRandom == 1 || angkaRandom == 2) {
+    // jika angkaRandom = 1 atau = 2, blok ini yang akan dijalankan
+    print('angka 1 atau 2');
+  } else if (angkaRandom == 3 || angkaRandom == 4) {
+    //jika angkaRandom = 3 atau = 4, blok ini yang akan dijalankan
+    print('angka 3 atau 4');
+  } else {
+//selain logika diatas, jika bukan 1 atau 2 atau 3 atau 4
+// akan masuk ke blok ini
+    print('pasti angka 5');
+  }
+}
