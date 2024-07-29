a83aceb644e2659722ca96fb019ba25a989635d2
Abdul Fatah <fatah03111995@gmail.com>
2024-07-29 10:51:21 +0700

    feat : pembahasan practice soal nomer 1

--- lib/lessons1/05_looping/practice.dart
+++ lib/lessons1/05_looping/practice.dart
@@ -1,4 +1,5 @@
 void main(List<String> args) {
+  //SOAL NOMER 1
   List<String> namaSiswa = ['fatIHIN', 'RizKI', 'fAtAh'];
   List<String> ubahJadiKecilSemua =
       []; //nilai benar ['fatihin', 'rizki', 'fatah']
@@ -8,6 +9,20 @@ void main(List<String> args) {
   for (int index = 0; index < panjangListNamaSiswa; index++) {
     //lakukan perulangan mulai dari index ke 0 sampai index yang nilainya < panjang nama siswa
     // < panjang nama siswa, artinya < 3, jadi akan dilakukan perulangan dari index = 0 sampai index = 2 (ingat < 3)
-    //TODO : LAKUKAN SESUATU DISINI SUPAYA VARIABLE UBAH JADI KECIL SEMUA BERISI NAMA SISWA YANG HURUFNYA DIUBAH JADI KECIL SEMUA
+    ubahJadiKecilSemua.add(namaSiswa[index].toLowerCase());
+
+    //PENJELASAN
+    //1. FUNGSI ADD AKAN DI ULANG SAMPAI INDEX KE 2 (INGAT < 3), INDEX KE 2 AKHIR DARI NAMA SISWA
+    //2. NILAI INDEX AKAN BERUBAH KARENA ADA INCREMENT (index++)
+
+    //3. PERHATIKAN SYNTAX JAWABAN
+    //namaSiswa[index], setiap perulangan index yang di akses akan berubah bermula dari index = 0 sampai index = 2
+    //misal. namaSiswa[0] adalah 'fatIHIN' string ini yang akan di 'add'
+    //toLowerString, INGAT PELAJARAN SEBELUMNYA
   }
+
+  //SOAL NOMER 2
+  List<String> ubahJadiKapitalDiAwal = [];
+
+  //TODO: LAKUKAN DENGAN FOR LOOP
 }
