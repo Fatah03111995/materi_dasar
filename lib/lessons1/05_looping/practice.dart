fd8705595913cf22a840a55c3d544babf5a146a6
Abdul Fatah <fatah03111995@gmail.com>
2024-07-29 10:36:42 +0700

    feat : tambaha penjelasan

--- lib/lessons1/05_looping/practice.dart
+++ lib/lessons1/05_looping/practice.dart
@@ -1,10 +1,13 @@
 void main(List<String> args) {
   List<String> namaSiswa = ['fatIHIN', 'RizKI', 'fAtAh'];
-  List<String> ubahJadiKecilSemua = [];
+  List<String> ubahJadiKecilSemua =
+      []; //nilai benar ['fatihin', 'rizki', 'fatah']
 
-  int panjangListNamaSiswa = namaSiswa.length;
+  int panjangListNamaSiswa = namaSiswa.length; // nilainya 3
 
   for (int index = 0; index < panjangListNamaSiswa; index++) {
-    //TODO LAKUKAN SESUATU DISINI SUPAYA VARIABLE UBAH JADI KECIL SEMUA BERISI NAMA SISWA YANG HURUFNYA DIUBAH JADI KECIL SEMUA
+    //lakukan perulangan mulai dari index ke 0 sampai index yang nilainya < panjang nama siswa
+    // < panjang nama siswa, artinya < 3, jadi akan dilakukan perulangan dari index = 0 sampai index = 2 (ingat < 3)
+    //TODO : LAKUKAN SESUATU DISINI SUPAYA VARIABLE UBAH JADI KECIL SEMUA BERISI NAMA SISWA YANG HURUFNYA DIUBAH JADI KECIL SEMUA
   }
 }
