42979db195f8011121b740f12e45daa16e266a96
Abdul Fatah <fatah03111995@gmail.com>
2024-07-29 10:33:31 +0700

    feat : practice

---
+++ lib/lessons1/05_looping/practice.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  List<String> namaSiswa = ['fatIHIN', 'RizKI', 'fAtAh'];
+  List<String> ubahJadiKecilSemua = [];
+
+  int panjangListNamaSiswa = namaSiswa.length;
+
+  for (int index = 0; index < panjangListNamaSiswa; index++) {
+    //TODO LAKUKAN SESUATU DISINI SUPAYA VARIABLE UBAH JADI KECIL SEMUA BERISI NAMA SISWA YANG HURUFNYA DIUBAH JADI KECIL SEMUA
+  }
+}
